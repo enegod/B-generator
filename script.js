a9eff3b52a9696e09dfe3e61002f66910ebcada9
enegod <32158490+enegod@users.noreply.github.com>
2020-09-26 10:39:10 +0100

    Adding starting project

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log(document);
+
+// var foodl = ["beans", "rice", "yam", "plantain"];
+
+// foodl.forEach(function(i) {
+// 	document.write(i + " ")
+// })
+
+function foo() {
+	"google"
+}
+foo()
